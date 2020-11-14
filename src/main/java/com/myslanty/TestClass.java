dc4cf6ed1a75f62b3b6e8874aca36d84a4af7cba
Amiran Kurman <amiranfox@gmail.com>
2020-11-15 01:22:14 +0600

    added: examples of sending and getting data

--- src/main/java/com/myslanty/TestClass.java
+++ src/main/java/com/myslanty/TestClass.java
@@ -2,10 +2,11 @@ package com.myslanty;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.json.Json;
+import javax.json.bind.annotation.JsonbProperty;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("amiran")
@@ -13,6 +14,28 @@ public class TestClass {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Person getString() {
-        return new Person("Amiran", "Kurman", 17);
+        Person p = new Person();
+        p.setAge(17);
+        p.setName("Amiran");
+        p.setSurname("Kurman");
+        return p;
+    }
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Person postString(Person person) {
+        return person;
+    }
+    @Path("usingjson")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Person postStringUsingJSON(String body) {
+        JsonObject json = new JsonParser().parse(body).getAsJsonObject();
+        Person p = new Person();
+        p.setAge(json.get("age").getAsInt());
+        p.setName(json.get("name").getAsString());
+        p.setSurname(json.get("surname").getAsString());
+        return p;
     }
 }
